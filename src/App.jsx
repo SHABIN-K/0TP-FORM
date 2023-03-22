a884e79ed2143bd69374210162f0395b47d49329
Shabin-k <helloshabink@gmail.com>
2023-03-22 23:03:28 +0530

    ux improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,70 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  const inputRef = useRef({})
+  const [otp, setOtp] = useState({
+    digitOne : "",
+    digitTwo : "",
+    digitThree : "",
+    digitFour : "",
+    digitFive : "",
+    digitSix : "",
+  })
+ 
+  useEffect(() => {
+    inputRef.current[0].focus();
+
+    inputRef.current[0].addEventListener("paste", pasteText);
+
+    return () => inputRef.current[0].removeEventListener("paste", pasteText);
+  }, [])
+
+  const pasteText = (event) => {
+    event.clipboardData.getData("text")
+  }
+  
+  const handleChange = (event, index) => {
+    const { name , value }  = event.target;
+
+    if(/[a-z]/gi.test(value)) return;
+
+    setOtp(prev => ({
+      ...prev,
+      [name] : value.slice(-1),
+
+    }))
+
+    if(value && index < 5)
+        inputRef.current[index +1].focus();
+  };
+
+  const handleBackSpace = (event, index) => {
+    if(event.key === "Backspace")
+      if(index > 0)
+        inputRef.current[index -1].focus();
+  }
 
   const renderInput =() =>{
-     return  <input type="text" className='w-16 h-12 rounded-md mr-3 text-center text-xl'/>
+    return Object.keys(otp).map(( keys, index) => (
+      <input 
+         type="text" 
+         key={index}
+         name={keys} 
+        // maxLength="1"
+         value={otp[keys]}
+         ref={(element) => (inputRef.current[index] = element)}
+         className='w-16 h-12 rounded-md mr-3 text-center text-xl' 
+         onChange={(event) => handleChange(event,index)}
+         onKeyUp={(event) => handleBackSpace(event,index)}
+        />
+    ));
   };
 
   return (
     <form action="">
       <h3 className='text-3xl mb-8 '>Please fill in the otp</h3>
-      <div>
-        {renderInput()}
-        {renderInput()}
-        {renderInput()}
-        {renderInput()}
-        {renderInput()}
-        {renderInput()}
-      </div>   
+      <div>{renderInput()}</div>   
       <button className='mt-4 w-34 border border-solid bg-[#3b3b3b] rounded hover:bg-[#252525] hover:border-[#3b3b3b3b]'>Submit</button>
     </form>
   )
