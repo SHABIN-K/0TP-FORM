56389320fdf42c1e8a31664c87f26d38f8566300
Shabin-k <helloshabink@gmail.com>
2023-03-22 21:31:34 +0530

    ui : ui development completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const renderInput =() =>{
+     return  <input type="text" className='w-16 h-12 rounded-md mr-3 text-center text-xl'/>
+  };
+
   return (
     <form action="">
-      <h3>Please fill in the otp</h3>
+      <h3 className='text-3xl mb-8 '>Please fill in the otp</h3>
       <div>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+        {renderInput()}
+        {renderInput()}
+        {renderInput()}
+        {renderInput()}
+        {renderInput()}
+        {renderInput()}
       </div>   
-      <button>Submit</button>
+      <button className='mt-4 w-34 border border-solid bg-[#3b3b3b] rounded hover:bg-[#252525] hover:border-[#3b3b3b3b]'>Submit</button>
     </form>
   )
 }
